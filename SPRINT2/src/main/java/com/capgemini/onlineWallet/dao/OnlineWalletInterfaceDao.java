1e70f50bf890f2bf2b6e422183b5441f7d8b879c
aishu-22 <51692680+aishu-22@users.noreply.github.com>
2020-04-30 03:12:12 +0530

    Delete OnlineWalletInterfaceDao.java

--- SPRINT2/src/main/java/com/capgemini/onlineWallet/dao/OnlineWalletInterfaceDao.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.onlineWallet.dao;
-
-public interface OnlineWalletInterfaceDao {
-
-	public boolean addMoney(int accountId, double amount);
-	public boolean sendMoney(int senderAccId, int receiverAccId, double amount);
-}
